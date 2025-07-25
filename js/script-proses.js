5af8a68c4cb5064cda85fa02a2f8f215bc74354a
mochamadedwin <mochamadedwinnurishak@gmail.com>
2025-07-25 18:07:05 +0700

    proses/pengolahan page

---
+++ js/script-proses.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const connectWA = document.getElementById("connectKontak");
+  connectWA.addEventListener("click", function () {
+    window.open("https://wa.me/62895393928966", "_blank");
+  });
+});
